c6440f0a1a6e149d4fafd4e30b287b40272cb726
huiju.ko <huiju.ko@m2digital.co.kr>
2025-06-24 15:22:58 +0900

    User dto

--- mmbs-was/src/main/java/com/mong/mmbs/entity/UserEntity.java
+++ mmbs-was/src/main/java/com/mong/mmbs/entity/UserEntity.java
@@ -2,10 +2,14 @@ package com.mong.mmbs.entity;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import com.mong.mmbs.dto.UserDto;
+
+import com.mong.mmbs.dto.SignUpDto;
+import com.mong.mmbs.dto.UserUpdateDto;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,60 +20,48 @@ import lombok.NoArgsConstructor;
 @Table(name="user")
 @Entity(name="user")
 public class UserEntity {
-    @Id
-    private String userId;
-    private String userPassword;
-    private String userEmail;
-    private String userAddress;
-    private String userAddressDetail;
-    private String userName;
-    private int roleId;
-    private String userPhone;
-    private String userKidBirth;
-    private String userSignUpDate;
-    private String userWithdraw;
-
-    // 회원가입 생성자
-    public UserEntity(UserDto dto) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        this.userId = dto.getUserId();
-        this.userPassword = dto.getUserPassword();
-        this.userEmail = dto.getUserEmail();
-        this.userAddress = dto.getUserAddress();
-        this.userAddressDetail = dto.getUserAddressDetail();
-        this.userName = dto.getUserName();
-        this.roleId = dto.getRoleId();
-        this.userPhone = dto.getUserPhone();
-        this.userKidBirth = dto.getUserKidBirth();
-        this.userSignUpDate = dateFormat.format(new Date());
-    }
-
-    // 업데이트 메서드 (선택적 필드 업데이트 가능)
-    public void updateFromDto(UserDto dto) {
-        if(dto.getUserPassword() != null && !dto.getUserPassword().isEmpty()) {
-            this.userPassword = dto.getUserPassword();
-        }
-        if(dto.getUserEmail() != null) {
-            this.userEmail = dto.getUserEmail();
-        }
-        if(dto.getUserAddress() != null) {
-            this.userAddress = dto.getUserAddress();
-        }
-        if(dto.getUserAddressDetail() != null) {
-            this.userAddressDetail = dto.getUserAddressDetail();
-        }
-        if(dto.getUserName() != null) {
-            this.userName = dto.getUserName();
-        }
-        if(dto.getRoleId() != 0) {
-            this.roleId = dto.getRoleId();
-        }
-        if(dto.getUserPhone() != null) {
-            this.userPhone = dto.getUserPhone();
-        }
-        if(dto.getUserKidBirth() != null) {
-            this.userKidBirth = dto.getUserKidBirth();
-        }
-        // userWithdraw는 보통 별도 로직으로 관리
-    }
+//	아이디
+	@Id
+	private String userId;
+//	비밀번호
+	private String userPassword;
+//	이메일
+	private String userEmail;
+//	주소
+	private String userAddress;
+//	상세 주소
+	private String userAddressDetail;
+//	이름
+	private String userName;
+//	전화번호
+	private String userPhone;
+//	아이 생일
+	private String userKidBirth;
+//	가입 날짜
+	private String userSignUpDate;
+//	탈퇴 날짜
+	private String userWithdraw;
+	
+	public UserEntity(SignUpDto dto) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		
+		this.userId = dto.getUserId();
+		this.userPassword = dto.getUserPassword();
+		this.userEmail = dto.getUserEmail();
+		this.userAddress = dto.getUserAddress();
+		this.userAddressDetail =dto.getUserAddressDetail();
+		this.userName = dto.getUserName();
+		this.userPhone = dto.getUserPhone();
+		this.userKidBirth = dto.getUserKidBirth();
+		this.userSignUpDate = dateFormat.format(new Date());
+	}
+	
+	public void setUpdateUser(UserUpdateDto dto) {
+		
+		this.userAddress = dto.getUserAddress();
+		this.userAddressDetail =dto.getUserAddressDetail();
+		this.userName = dto.getUserName();
+		this.userPhone = dto.getUserPhone();
+		this.userKidBirth = dto.getUserKidBirth();
+	}
 }
--- mmbs-was/src/main/java/com/mong/mmbs/service/UserService.java
+++ mmbs-was/src/main/java/com/mong/mmbs/service/UserService.java
@@ -1,74 +1,71 @@
 package com.mong.mmbs.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mong.mmbs.dto.ResponseDto;
-import com.mong.mmbs.dto.UserDto;
+import com.mong.mmbs.dto.UserDeleteDto;
+import com.mong.mmbs.dto.UserUpdateDto;
 import com.mong.mmbs.entity.UserEntity;
 import com.mong.mmbs.repository.UserRepository;
 
 @Service
 public class UserService {
-
-    private final UserRepository userRepository;
-
-    // 스프링 4.3 이상부터는 생성자가 딱 1개 있으면 @Autowired가 없어도 자동 주입됨
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
+	
+	@Autowired UserRepository userRepository;
+	
     public ResponseDto<?> getUser(String userId) {
-        UserEntity user = null;
-        try {
-            user = userRepository.findByUserId(userId);
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            return ResponseDto.setFailed("Database Error");
-        }
-
-        if (user != null) user.setUserPassword(""); // 비밀번호 숨김 처리
-
-        return ResponseDto.setSuccess("result", user);
+    	UserEntity user = null;
+    	try {
+    		user = userRepository.findByUserId(userId);
+    	} catch (Exception exception) {
+    		return ResponseDto.setFailed("Database Error");
+    	}
+    	
+    	if (user != null) user.setUserPassword("");
+    	
+    	return ResponseDto.setSuccess("result", user);
     }
+    
+	public ResponseDto<?> userUpdate(UserUpdateDto dto){
+		
+		UserEntity user = null;
+		
+		try {
+			user = userRepository.findByUserId(dto.getUserId());
+			if (user == null) ResponseDto.setFailed("Does Not Exist User");
+		} catch (Exception exception) {
+			ResponseDto.setFailed("Failed");
+		}
+		
+		user.setUpdateUser(dto);
+		
+		try {
+			userRepository.save(user);
+		} catch (Exception exception) {
+			ResponseDto.setFailed("Failed");
+		}
+		
+		return ResponseDto.setSuccess("Sucess", user);
+	}
 
-    public ResponseDto<?> userUpdate(UserDto dto) {
-        UserEntity user = null;
+	public ResponseDto<?> userDelete (String userId, UserDeleteDto dto) {
+		
+		userId = dto.getUserId();
+		String userEmail = dto.getUserEmail();
 
-        try {
-            user = userRepository.findByUserId(dto.getUserId());
-            if (user == null) return ResponseDto.setFailed("Does Not Exist User");
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            return ResponseDto.setFailed("Failed");
-        }
+		UserEntity userEntity = null; 
 
-        user.updateFromDto(dto);
-
-        try {
-            userRepository.save(user);
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            return ResponseDto.setFailed("Failed");
-        }
-
-        return ResponseDto.setSuccess("Success", user);
-    }
-
-    public ResponseDto<?> userDelete(String userId, UserDto dto) {
-        try {
-            if (!userRepository.existsByUserIdAndUserEmail(userId, dto.getUserEmail())) {
-                return ResponseDto.setFailed("UserId Or UserEmail Does Not Exist");
-            }
-
-            UserEntity userEntity = userRepository.findByUserId(userId);
-            if (userEntity == null) return ResponseDto.setFailed("User Not Found");
-
-            userRepository.delete(userEntity);
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            return ResponseDto.setFailed("Failed");
-        }
-
-        return ResponseDto.setSuccess("Success", null);
-    }
+		try {
+			if(!userRepository.existsByUserIdAndUserEmail(userId, userEmail))
+				return ResponseDto.setFailed("UserId Or UserEmail Does Not Exist");
+				
+			userEntity = userRepository.findByUserId(userId);
+			userRepository.delete(userEntity);
+		} catch (Exception exception) {
+			exception.printStackTrace();
+			return ResponseDto.setFailed("Failed");
+		}
+		return ResponseDto.setSuccess("Success", null);
+	}
 }
